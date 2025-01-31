af46af0e3e6417c3970f769528538f45a79241bc
hoodey <41493055+hoodey@users.noreply.github.com>
2025-01-30 20:53:43 -0600

    Flashlight, skybox updated
    Fully functioning flashlight prototype. Night skybox.

--- Assets/Scripts/Player/Flashlight.cs
+++ Assets/Scripts/Player/Flashlight.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Flashlight : MonoBehaviour
 {
+    [SerializeField] Light flashLight;
+    bool FlashlightOn = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,21 @@ public class Flashlight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F) && FlashlightOn == true)
+        {
+            Debug.Log("F key was pressed.");
+            flashLight.intensity = 0.0f;
+            FlashlightOn = false;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.F) && FlashlightOn == false)
+        {
+
+            Debug.Log("F key was pressed.");
+            flashLight.intensity = 1.0f;
+            FlashlightOn = true;
+        }
+
     }
+
 }
