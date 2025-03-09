2290014cb580b91b6a9c3863f1702fa055971492
Luka Lux <156493124+50uk450u10@users.noreply.github.com>
2025-03-09 01:26:20 -0600

    implemented flashlight and enemy logic

--- Assets/Scripts/HideScript.cs
+++ Assets/Scripts/HideScript.cs
@@ -6,15 +6,15 @@ public class HideScript : MonoBehaviour
     PlayerController player = null;
     Interactable obj;
     EnemyAI enemy;
+    Flashlight flashlight;
     Vector3 spotPos;
     Vector3 playerPos;
     float playerSpeed;
-    //float enemyDetect;
+    float enemyDetect;
     float hideValue = 0f;
 
     void Start()
     {
-        //enemy = FindAnyObjectByType<EnemyAI>();
         obj = GetComponent<Interactable>();
         spotPos = this.transform.position;
     }
@@ -30,22 +30,31 @@ public class HideScript : MonoBehaviour
         {
             if (player != null && player.isHiding == false)
             {
-                Debug.Log("hide");
+                enemy = FindAnyObjectByType<EnemyAI>();
+                if(enemy != null)
+                {
+                    enemyDetect = enemy.detectionRange;
+                    enemy.detectionRange = hideValue;
+                }
+
+                flashlight = FindAnyObjectByType<Flashlight>();
+                if (flashlight != null) { Debug.Log("flash"); flashlight.FlashlightOn = false; }
+
                 playerPos = player.transform.position;
                 player.transform.position = spotPos;
                 playerSpeed = player.speed;
                 player.speed = hideValue;
-                //enemyDetect = enemy.detectionRange;
-                //enemy.detectionRange = hideValue;
                 player.isHiding = true;
             }
 
             else if (player != null && player.isHiding == true)
             {
-                Debug.Log("unhide");
+                if (enemy != null) { enemy.detectionRange = enemyDetect; }
+
+                if (flashlight != null) { flashlight.FlashlightOn = true; }
+
                 player.transform.position = playerPos;
                 player.speed = playerSpeed;
-                //enemy.detectionRange = enemyDetect;
                 player.isHiding = false;
             }
         }
