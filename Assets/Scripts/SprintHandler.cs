c6d4539a24c947af5aa9fa992b12eb296b7b95bd
WheelchairBound64 <123192532+WheelchairBound64@users.noreply.github.com>
2025-03-11 00:45:16 -0500

    Sprint Finished
    Sprint bar finalized and fully functional, adjusted sensitivity UI panel to be slightly higher

--- Assets/Scripts/SprintHandler.cs
+++ Assets/Scripts/SprintHandler.cs
@@ -1,64 +1,75 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SprintHandler : MonoBehaviour
 {
-    [SerializeField] float sprintSpeed = 6; // allows editor to set sprint speed
-    [SerializeField] float sprintDuration = 3; // sets how long (in seconds) the sprint lasts
-    private float scriptOnlySprintDuration;
-    [SerializeField] float sprintCooldown = 5; // sets how long (in seconds) the sprint takes to fully recharge
-    private float scriptOnlySprintCooldown;
-    private bool canSprint;
+    [SerializeField] float sprintSpeed = 10; // allows editor to set sprint speed
+    [SerializeField] float sprintCost = 33; // sets how much of the total stamina is taken per second
+    [SerializeField] float sprintCooldown = 2.5f; // sets how long (in seconds) the sprint takes before recharging
+    [SerializeField] float sprintRechargeRate = 20; // sets how much stamina is replenished a second
+    [SerializeField] float maxStamina = 100; // sets the maximum stamina the players has
+    [SerializeField] float currentStamina; //shows current stamina the player has
+    [SerializeField] bool isSprinting;
     PlayerController playerController;
+    [SerializeField] Image sprintBar;
+    private Coroutine recharge;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerController = GetComponent<PlayerController>();
-        canSprint = true;
+        isSprinting = false;
+        currentStamina = maxStamina;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift) && canSprint == true)
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            canSprint = false;
-            StartSprinting();
+            if (currentStamina > 0)
+            {
+                isSprinting = true;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isSprinting = false;
+            playerController.playerSpeed = playerController.speed;
+        }
+
+        if (isSprinting == true)
+        {
+            playerController.playerSpeed = sprintSpeed;
+            currentStamina -= sprintCost * Time.deltaTime;
+            if (currentStamina < 0 || currentStamina == 0)
+            {
+                currentStamina = 0;
+                isSprinting = false;
+                playerController.playerSpeed = playerController.speed;
+            }
+            sprintBar.fillAmount = currentStamina/maxStamina;
+            if(recharge != null)
+            {
+                StopCoroutine(recharge);
+            }
+            recharge = StartCoroutine(RechargeStamina());
         }
     }
 
-    private void StartSprinting()
+    private IEnumerator RechargeStamina()
     {
-        playerController.playerSpeed = sprintSpeed;
-        scriptOnlySprintDuration = sprintDuration;
-        scriptOnlySprintCooldown = sprintCooldown;
-        StartCoroutine(Sprinting());
-    }
+        yield return new WaitForSeconds(sprintCooldown);
 
-    IEnumerator Sprinting()
-    {
-        Debug.Log("Sprinting at " + playerController.playerSpeed + " speed");
-        while (scriptOnlySprintDuration > 0)
+        while(currentStamina < maxStamina)
         {
-            scriptOnlySprintDuration -= .1f;
-            yield return new WaitForSeconds(.1f);
+            currentStamina += sprintRechargeRate / 20f;
+            if (currentStamina > maxStamina)
+            {
+                currentStamina = maxStamina;
+            }
+            sprintBar.fillAmount = currentStamina / maxStamina;
+            yield return new WaitForSeconds(.05f);
         }
-        playerController.playerSpeed = playerController.speed;
-        StartCoroutine(SprintRecharge());
-        scriptOnlySprintDuration = sprintDuration;
-        Debug.Log("Sprint ends, speed is back to " + playerController.playerSpeed);
-    }
-
-    IEnumerator SprintRecharge()
-    {
-        Debug.Log("Recharging sprint");
-        while (scriptOnlySprintCooldown > 0)
-        {
-            scriptOnlySprintCooldown -= .1f;
-            yield return new WaitForSeconds(.1f);
-        }
-        canSprint = true;
-        scriptOnlySprintCooldown = sprintCooldown;
-        Debug.Log("Sprint ready");
     }
 }
