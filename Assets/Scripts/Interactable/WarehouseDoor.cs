0567eb0b09a78cc14ada23b672384da68420b548
hoodey <41493055+hoodey@users.noreply.github.com>
2025-04-17 15:34:32 -0500

    Setup general outside game loop
    added the gates and created a script to enter the warehouse. Added the items to get open the doors / gate

---
+++ Assets/Scripts/Interactable/WarehouseDoor.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class WarehouseDoor : MonoBehaviour
+{
+    [SerializeField] string warehouseScene;
+    [SerializeField] string requiredItem;
+
+    private Inventory playerInventory;
+    private void Start()
+    {
+        playerInventory = FindFirstObjectByType<Inventory>();
+    }
+
+    public void EnterBuilding()
+    {
+        foreach (Item k in playerInventory.items) //Loop through our inventory looking for the item to open the chest
+        {
+            if (k.itemName == requiredItem)
+            {
+                SceneManager.LoadScene(warehouseScene); //Load into the warehouse
+            }
+        }
+    }
+}
