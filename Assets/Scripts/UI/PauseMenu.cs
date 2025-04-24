b40cb446205394b1e02a18a52743addd3442ee8c
WheelchairBound64 <123192532+WheelchairBound64@users.noreply.github.com>
2025-04-23 21:51:39 -0500

    PauseMenu tweaked
    Added settings panel where sensitivity can be tweaked and controls are displayed
    
    Added confirm quit panel to alert player that quitting results in lost progress, as well as made the quit return to main menu and not quit the game

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-
+    private bool quitButtonPressed = false;
+    private bool settingsButtonPressed = false;
     public GameObject pauseMenuUI;
+    public GameObject confirmQuitPanel;
+    public GameObject buttonPanel;
+    public GameObject settingsPanel;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (GameIsPaused)
+            if (GameIsPaused && !settingsButtonPressed)
             {
                 Resume();
+            }
+            else if(settingsButtonPressed)
+            {
+                Settings();
             }
             else
             {
@@ -28,15 +37,26 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1.0f;
-        GameIsPaused = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        if (quitButtonPressed)
+        {
+            confirmQuitPanel.SetActive(false);
+            buttonPanel.SetActive(true);
+            quitButtonPressed = false;
+        }
+        else
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1.0f;
+            GameIsPaused = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            quitButtonPressed = false;
+        }
     }
 
     public void Pause()
     {
         pauseMenuUI.SetActive(true);
+        buttonPanel.SetActive(true);
         Time.timeScale = 0;
         GameIsPaused = true;
         Cursor.lockState = CursorLockMode.Confined;
@@ -44,9 +64,34 @@ public class PauseMenu : MonoBehaviour
 
     public void Quit()
     {
-        Application.Quit();
-        Debug.Log("Game quit");
+        if (!quitButtonPressed)
+        {
+            quitButtonPressed = true;
+            confirmQuitPanel.SetActive(true);
+            buttonPanel.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+            quitButtonPressed = false;
+            SceneManager.LoadScene(0);      //sends player back to main menu
+        }
     }
 
+    public void Settings()
+    {
+        if (settingsButtonPressed)
+        {
+            settingsButtonPressed = false;
+            settingsPanel.SetActive(false);
+            buttonPanel.SetActive(true);
+        }
+        else
+        { 
+            settingsButtonPressed = true;
+            settingsPanel.SetActive(true);
+            buttonPanel.SetActive(false);
+        }
+    }
 
 }
