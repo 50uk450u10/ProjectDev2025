6b9b2c80a743de85f0c4f7f083e9b3267adcfe86
hoodey <41493055+hoodey@users.noreply.github.com>
2025-04-25 15:24:12 -0500

    Revert "Merge branch 'Clark-Dev-New' into Blake-Branch"
    This reverts commit f1276c7d24df494f4547dca0f95213bd74b2c5fb, reversing
    changes made to 4e87d56cf9a504180a2cd6403ba18bb7e957ba43.

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -8,25 +8,17 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-    private bool quitButtonPressed = false;
-    private bool settingsButtonPressed = false;
+
     public GameObject pauseMenuUI;
-    public GameObject confirmQuitPanel;
-    public GameObject buttonPanel;
-    public GameObject settingsPanel;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (GameIsPaused && !settingsButtonPressed)
+            if (GameIsPaused)
             {
                 Resume();
-            }
-            else if(settingsButtonPressed)
-            {
-                Settings();
             }
             else
             {
@@ -37,26 +29,15 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume()
     {
-        if (quitButtonPressed)
-        {
-            confirmQuitPanel.SetActive(false);
-            buttonPanel.SetActive(true);
-            quitButtonPressed = false;
-        }
-        else
-        {
-            pauseMenuUI.SetActive(false);
-            Time.timeScale = 1.0f;
-            GameIsPaused = false;
-            Cursor.lockState = CursorLockMode.Locked;
-            quitButtonPressed = false;
-        }
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void Pause()
     {
         pauseMenuUI.SetActive(true);
-        buttonPanel.SetActive(true);
         Time.timeScale = 0;
         GameIsPaused = true;
         Cursor.lockState = CursorLockMode.Confined;
@@ -64,34 +45,11 @@ public class PauseMenu : MonoBehaviour
 
     public void Quit()
     {
-        if (!quitButtonPressed)
-        {
-            quitButtonPressed = true;
-            confirmQuitPanel.SetActive(true);
-            buttonPanel.SetActive(false);
-        }
-        else
-        {
-            Time.timeScale = 1.0f;
-            quitButtonPressed = false;
-            SceneManager.LoadScene(0);      //sends player back to main menu
-        }
+        //Go To Main Menu
+        SceneManager.LoadScene("MainMenu");
+        //Application.Quit();
+        //Debug.Log("Game quit");
     }
 
-    public void Settings()
-    {
-        if (settingsButtonPressed)
-        {
-            settingsButtonPressed = false;
-            settingsPanel.SetActive(false);
-            buttonPanel.SetActive(true);
-        }
-        else
-        { 
-            settingsButtonPressed = true;
-            settingsPanel.SetActive(true);
-            buttonPanel.SetActive(false);
-        }
-    }
 
 }
