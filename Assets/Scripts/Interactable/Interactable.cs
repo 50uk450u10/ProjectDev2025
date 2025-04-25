d95ecbd833333c65d481cb764e8afd7e2db010a6
hoodey <41493055+hoodey@users.noreply.github.com>
2025-04-25 11:57:48 -0500

    Highlighting objects is in game!
    It looks really good.

--- Assets/Scripts/Interactable/Interactable.cs
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -3,18 +3,18 @@ using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
+    private Canvas interactCanvas; //The canvas where "Press E" text appears
     private PlayerController player = null;
+    private Outline outline;
     public UnityEvent onPerformInteraction; //Events to perform on Trigger Enter
     public UnityEvent onEndInteraction; //Events to perform on Trigger Exit
     public bool isHidingSpot;
-    Canvas interactCanvas; //The canvas where "Press E" text appears
     
-
     void Start()
     {
         var canvasParent = GameObject.Find("InteractionCanvas");
         interactCanvas = canvasParent.GetComponent<Canvas>();
-
+        outline = GetComponent<Outline>();
     }
 
     //Invoke Unity Event to perform interaction logic
@@ -32,7 +32,11 @@ public class Interactable : MonoBehaviour
     {
         player = other.GetComponent<PlayerController>(); //Grab player component from collider's gameobject
         interactCanvas.enabled = true; //Turn on our text prompt
-        
+
+        if (outline != null)
+        {
+            outline.enabled = true;
+        }
 
         if (player != null)
         {
@@ -47,6 +51,10 @@ public class Interactable : MonoBehaviour
         player = other.GetComponent<PlayerController>(); //Grab player component from collider's gameobject
         interactCanvas.enabled = false; //Turn off our text prompt
 
+        if (outline != null)
+        {
+            outline.enabled = false;
+        }
 
         if (player != null)
         {
@@ -58,6 +66,11 @@ public class Interactable : MonoBehaviour
 
     private void OnDestroy()
     {
+        if (outline != null)
+        {
+            outline.enabled = false;
+        }
+
         if (player != null)
         {
             player.on_InteractPressed -= PerformInteraction;
