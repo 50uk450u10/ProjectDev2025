94d4c7d956fd380e3a436445ee2a29c104974658
AriannaStorm <ariannastorm@outlook.com>
2025-04-27 13:31:20 -0500

    fixed doors and added clutter

--- Assets/Scripts/Interactable/BasicDoorScript.cs
+++ Assets/Scripts/Interactable/BasicDoorScript.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicDoorScript : MonoBehaviour
+
+// Main door script that makes the door open when pressing E
 {
     public GameObject Notif;
-    public Animator Door;
+    public LayerMask mask;
 
     void Update() 
     {
         if(Input.GetKeyDown(KeyCode.E) && Notif.activeInHierarchy == true)
         {
-            Door.SetTrigger("Open");
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, mask))
+            {
+                hit.collider.gameObject.transform.parent.gameObject.GetComponent<Animator>().SetTrigger("Open");
+            }
         }
-        
     }
 
+// Turns on the UI that tells the player to Press E to open door
     void OnTriggerEnter(Collider obj) 
     {
         if(obj.tag == "Door")
@@ -24,6 +30,7 @@ public class BasicDoorScript : MonoBehaviour
         }
     }
 
+// Turns off the UI that tells player to Press E to open door 
     void OnTriggerExit(Collider obj) 
     {
         if(obj.tag == "Door")
