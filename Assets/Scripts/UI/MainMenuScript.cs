c34df1c1548a7eaa8238fd46053e9ceeac47b60c
WheelchairBound64 <123192532+WheelchairBound64@users.noreply.github.com>
2025-04-30 00:48:54 -0500

    Settings Tab
    Added a settings tab to the main menu and pause menu

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -4,10 +4,26 @@ using UnityEngine.SceneManagement;
 public class MainMenuScript : MonoBehaviour
 {
     [SerializeField] string sceneToLoad;        //attach scene's name to vairable and game should run said scene
+    public GameObject settingsPanel;
+    private bool settingsPanelOpen = false;
 
     public void StartGame()
     {
         SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void SettingsTab()
+    {
+        if(settingsPanelOpen)
+        {
+            settingsPanel.SetActive(false);
+            settingsPanelOpen = false;
+        }
+        else
+        {
+            settingsPanel.SetActive(true);
+            settingsPanelOpen = true;
+        }
     }
 
     public void ExitGame()
