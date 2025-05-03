0e6185bb70ff72d99a39317ca2e34927072f008c
hoodey <41493055+hoodey@users.noreply.github.com>
2025-05-02 19:05:04 -0500

    Updated scripts in order to remove bolt cutters
    from inventory when they are used

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour
 {
     public List<Item> items; //Create a list to hold items that get picked up
+    public List<GameObject> icons;
     public int itemsInInventory = 0; //An int to easily track how many items we have
     [SerializeField] GameObject PanelParent; //Get reference to the UI Panel that holds our items
     [SerializeField] GameObject PickupPrefab; //Get a reference to a default pick up prefab for our UI
@@ -16,13 +17,18 @@ public class Inventory : MonoBehaviour
         itemsInInventory++;
         GameObject itemAdded;
         itemAdded = Instantiate(PickupPrefab, PanelParent.transform);
+        itemAdded.name = item.name;
         Image myImage = itemAdded.GetComponent<Image>();
         myImage.sprite = item.sprite;
+        icons.Add(itemAdded);
         item.gameObject.SetActive(false);
     }
 
-    public void RemoveItem(Item item) //public function to be called when removing an item from inventory (currently unused)
+    public void RemoveItem(Item item) //public function to be called when removing an item from inventory
     {
+
+        GameObject g = GameObject.Find(item.name);
+        Destroy(g);
         items.Remove(item);
         itemsInInventory--;
     }
